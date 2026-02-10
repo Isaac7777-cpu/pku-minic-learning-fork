8b7efd4bfeec6e95210b9df902dc19db2f0cf895
Isaac Leong <isaacleong23367@gmail.com>
2026-02-11 01:41:48 +1100

    Migrate: Migration of codegen_ctx to use the new reg

--- cpp/src/codegen_ctx.cpp
+++ cpp/src/codegen_ctx.cpp
@@ -1,11 +1,12 @@
 #include "codegen_ctx.hpp"
 #include "logger.hpp"
+#include "reg.hpp"
 
-bool CodeGenCtx::is_avail(const reg_t &reg) {
-  if (reg.series == 't') {
-    return this->t_reg[reg.idx];
-  } else if (reg.series == 'a') {
-    return this->a_reg[reg.idx];
+bool CodeGenCtx::is_avail(const rv::Reg &reg) {
+  if (reg.series() == 't') {
+    return this->t_reg[reg.idx()];
+  } else if (reg.series() == 'a') {
+    return this->a_reg[reg.idx()];
   } else {
     LOG_ERROR("Unrecognised register series.");
   }
@@ -15,8 +16,8 @@ bool CodeGenCtx::is_avail(const reg_t &reg) {
 // 1. T-registers
 // 2. A-registers
 // but does not guarantee which one is free first.
-std::optional<reg_t> CodeGenCtx::get_avail() {
-  std::optional<reg_t> t_reg = get_avail('t');
+std::optional<rv::Reg> CodeGenCtx::get_avail() {
+  std::optional<rv::Reg> t_reg = get_avail('t');
   if (!t_reg) {
     return get_avail('a');
   } else {
@@ -24,12 +25,12 @@ std::optional<reg_t> CodeGenCtx::get_avail() {
   }
 }
 
-std::optional<reg_t> CodeGenCtx::get_avail(const char &series) {
+std::optional<rv::Reg> CodeGenCtx::get_avail(const char &series) {
   if (series == 't') {
     for (int i = 0; i < 8; i++) {
       if (!t_reg[i]) {
         t_reg[i] = true;
-        return reg_t{'t', i};
+        return rv::Reg(rv::T{i});
       }
     }
     return std::nullopt;
@@ -37,7 +38,7 @@ std::optional<reg_t> CodeGenCtx::get_avail(const char &series) {
     for (int i = 1; i < 9; i++) {
       if (!a_reg[i % 8]) {
         a_reg[i % 8] = true;
-        return reg_t{'a', i};
+        return rv::Reg(rv::A{i});
       }
     }
     return std::nullopt;
--- cpp/src/codegen_ctx.hpp
+++ cpp/src/codegen_ctx.hpp
@@ -1,24 +1,9 @@
 #pragma once
 
 #include <optional>
-#include <string>
 #include <unordered_map>
 #include "koopa.h"
-
-struct reg_t {
-  char series;
-  int idx;
-
-  bool operator==(const reg_t &other) const {
-    return (series == other.series && idx == other.idx);
-  }
-
-  bool operator!=(const reg_t &other) const { return !(*this == other); }
-
-  std::string to_string() const {
-    return std::string(1, series) + std::to_string(idx);
-  }
-};
+#include "reg.hpp"
 
 class CodeGenCtx {
 private:
@@ -27,9 +12,9 @@ private:
   bool a_reg[8];
 
 public:
-  std::unordered_map<koopa_raw_value_t, reg_t> reg_dict;
+  std::unordered_map<koopa_raw_value_t, rv::Reg> reg_dict;
 
-  bool is_avail(const reg_t &);
-  std::optional<reg_t> get_avail();
-  std::optional<reg_t> get_avail(const char &);
+  bool is_avail(const rv::Reg &);
+  std::optional<rv::Reg> get_avail();
+  std::optional<rv::Reg> get_avail(const char &);
 };
