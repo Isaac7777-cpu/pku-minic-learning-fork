fe91583eb7beeaae518224958a32b77c1311f997
Isaac Leong <isaacleong23367@gmail.com>
2026-02-11 01:41:01 +1100

    Added Register class that represents register better

---
+++ cpp/src/reg.cpp
@@ -0,0 +1,64 @@
+#include "reg.hpp"
+#include <stdexcept>
+#include <variant>
+
+namespace rv {
+
+bool operator==(const Reg &lhs, const Reg &rhs) { return lhs.v_ == rhs.v_; }
+bool operator!=(const Reg &lhs, const Reg &rhs) { return !(lhs == rhs); }
+
+namespace { // anonymous namespace: hidden in this .cpp only
+
+template <class... Ts> struct Overloaded : Ts... {
+  using Ts::operator()...;
+};
+template <class... Ts> Overloaded(Ts...) -> Overloaded<Ts...>;
+
+} // namespace
+
+std::string Reg::to_string() const {
+  return std::visit(Overloaded{
+                        [](const Zero &) { return std::string("x0"); },
+                        [](const Ra &) { return std::string("ra"); },
+                        [](const Sp &) { return std::string("sp"); },
+                        [](const Gp &) { return std::string("gp"); },
+                        [](const Tp &) { return std::string("tp"); },
+                        [](const T &r) { return "t" + std::to_string(r.idx); },
+                        [](const A &r) { return "a" + std::to_string(r.idx); },
+                        [](const S &r) { return "s" + std::to_string(r.idx); },
+                    },
+                    v_);
+}
+
+unsigned int Reg::idx() const {
+  return std::visit(
+      [](auto const &r) -> unsigned int {
+        using R = std::decay_t<decltype(r)>;
+        if constexpr (std::is_same_v<R, T> || std::is_same_v<R, A> ||
+                      std::is_same_v<R, S>) {
+          return r.idx;
+        } else {
+          throw std::runtime_error("Reg::idx() only valid for T/A/S");
+        }
+      },
+      v_);
+}
+
+char Reg::series() const {
+  return std::visit(
+      [](auto const &r) -> char {
+        using R = std::decay_t<decltype(r)>;
+        if constexpr (std::is_same_v<R, T>) {
+          return 'a';
+        } else if (std::is_same_v<R, A>) {
+          return 'a';
+        } else if (std::is_same_v<R, S>) {
+          return 's';
+        } else {
+          throw std::runtime_error("Reg::series() only valid for T/A/S");
+        }
+      },
+      v_);
+}
+
+} // namespace rv
---
+++ cpp/src/reg.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+#include <variant>
+
+namespace rv {
+
+struct Zero {};
+struct Ra {};
+struct Sp {};
+struct Gp {};
+struct Tp {};
+struct T {
+  int idx;
+};
+struct A {
+  int idx;
+};
+struct S {
+  int idx;
+};
+
+class Reg {
+public:
+  using Impl = std::variant<Zero, Ra, Sp, Gp, Tp, T, A, S>;
+
+  Reg(Zero v) : v_(v) {}
+  Reg(Ra v) : v_(v) {}
+  Reg(Sp v) : v_(v) {}
+  Reg(Gp v) : v_(v) {}
+  Reg(Tp v) : v_(v) {}
+  Reg(T v) : v_(v) {}
+  Reg(A v) : v_(v) {}
+  Reg(S v) : v_(v) {}
+
+  std::string to_string() const;
+  unsigned int idx() const;
+  char series() const;
+
+  friend bool operator==(const Reg& lhs, const Reg& rhs);
+  friend bool operator!=(const Reg& lhs, const Reg& rhs);
+
+private:
+  Impl v_;
+};
+
+} // namespace rv
