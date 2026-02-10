f04e509f8f0d2a769a833515d4fdd96a82a4671f
Isaac Leong <isaacleong23367@gmail.com>
2026-02-10 14:50:42 +1100

    Implemented Translation to KoopaIR

--- cpp/src/ir_builder.cpp
+++ cpp/src/ir_builder.cpp
@@ -20,25 +20,154 @@ koopa_ast::Value *translate_exp_c_ast(const c_ast::ExpAST &,
                                       koopa_ast::BasicBlock &);
 koopa_ast::Value *translate_primary_exp_c_ast(const c_ast::PrimaryAST &,
                                               koopa_ast::BasicBlock &);
-koopa_ast::Value *translate_unary_exp_c_ast(const c_ast::UnaryExpAST &,
-                                            koopa_ast::BasicBlock &);
 koopa_ast::Integer *translate_number_c_ast(const c_ast::NumberAST &,
                                            koopa_ast::BasicBlock &);
+koopa_ast::Value *translate_unary_exp_c_ast(const c_ast::UnaryExpAST &,
+                                            koopa_ast::BasicBlock &);
+koopa_ast::Value *translate_mul_exp_c_ast(const c_ast::MulExpAST &,
+                                          koopa_ast::BasicBlock &);
+koopa_ast::Value *translate_add_exp_c_ast(const c_ast::AddExpAST &,
+                                          koopa_ast::BasicBlock &);
 
 /*******************************************************************************
  *  Implementation Details for going from each C AST nodes to Koopa Node.      *
  *******************************************************************************/
 
 /**
- * Going from NumberAST to koopa Integer
+ * Converting a CompUnitAST in C to a program in koopa.
  */
-// std::unique_ptr<koopa_ast::Integer>
-koopa_ast::Integer *translate_number_c_ast(const c_ast::NumberAST &number,
-                                           koopa_ast::BasicBlock &block) {
-  auto *ret = block.Make<koopa_ast::Integer>(false, number.int_val);
+std::unique_ptr<koopa_ast::Program>
+translate_comp_unit_c_ast(const c_ast::CompUnitAST &comp_unit) {
+  auto ret = std::make_unique<koopa_ast::Program>();
+
+  auto *func_def =
+      dynamic_cast<const c_ast::FuncDefAST *>(comp_unit.func_def.get());
+  if (!func_def) {
+    throw std::runtime_error(
+        "ir_builder error: CompUnitAST expects FuncDefAST at param `func_def`");
+  }
+
+  ret->functions.push_back(translate_func_def_c_ast(*func_def));
+
   return ret;
 }
 
+/**
+ * Converting a FuncDefAST to a koopa function as the following:
+ *
+ *
+ * ```
+ *
+ *  c_ast::FuncDefAST:name (string)
+ *    -> koopa_ast::Function:name (string)
+ *
+ *  c_ast::FuncDefAST:type (string)
+ *    -> koopa_ast::Function:type (koopa_ast::Type)
+ *
+ *  c_ast::FuncDefAST:block (c_ast::BlockAST)
+ *    -> koopa_ast::Function:basicblocks (vector<koopa_ast::BasicBlock>)
+ *
+ * ```
+ *
+ *
+ * NOTE: I think this does satisfy the definition of a koopa block as it does
+ * only have one point of entry and only leave or return. However, this may not
+ * be the most optimised way to write this, of course.
+ */
+std::unique_ptr<koopa_ast::Function>
+translate_func_def_c_ast(const c_ast::FuncDefAST &func_def) {
+  auto ret = std::make_unique<koopa_ast::Function>();
+
+  // Get the function name
+  ret->name = "@" + func_def.ident;
+
+  // Get the type
+  auto *type =
+      dynamic_cast<const c_ast::FuncTypeAST *>(func_def.func_type.get());
+  if (!type) {
+    throw std::runtime_error("ir_builder error: FuncDefAST expects "
+                             "FuncTypeAST at param `func_type`");
+  }
+
+  ret->type = translate_func_type_c_ast(*type);
+
+  // Get the block
+  auto *block = dynamic_cast<const c_ast::BlockAST *>(func_def.block.get());
+  if (!block) {
+    throw std::runtime_error(
+        "ir_builder error: FuncDefAST expects BlockAST at param `block`");
+  }
+
+  // HACK: We only have one block for now.
+  ret->basicblocks.push_back(translate_block_c_ast(*block, "%entry"));
+
+  return ret;
+}
+
+/**
+ * Converting FuncType C AST to just Type koopa IR reps.
+ *
+ * HACK: Currently since we don't have type for the c_ast,
+ * this function will always return i32.
+ */
+std::unique_ptr<koopa_ast::Type>
+translate_func_type_c_ast(const c_ast::FuncTypeAST &func_type) {
+  // HACK: Currently it is just hard matching as it has to be int
+  return std::make_unique<koopa_ast::Type>(koopa_ast::Type::I32());
+}
+
+/**
+ * Converting a Block C AST to koopa Block for now.
+ *
+ * NOTE: The meaning of a block in C and a block in koopa is different.
+ */
+std::unique_ptr<koopa_ast::BasicBlock>
+translate_block_c_ast(const c_ast::BlockAST &block, std::string name) {
+  auto ret = std::make_unique<koopa_ast::BasicBlock>(name);
+
+  auto *stmt = dynamic_cast<const c_ast::StmtAST *>(block.stmt.get());
+  if (!stmt)
+    throw std::runtime_error(
+        "ir_builder error: BlockAST expects to have StmtAST at param `stmt`");
+
+  translate_stmt_c_ast(*stmt, *ret.get());
+
+  return ret;
+}
+
+/**
+ * Going from StmtAST to koopa stmt
+ *
+ */
+koopa_ast::Value *translate_stmt_c_ast(const c_ast::StmtAST &stmt,
+                                       koopa_ast::BasicBlock &block) {
+  // Cast and check that it has the correct type
+  auto *exp = dynamic_cast<const c_ast::ExpAST *>(stmt.exp.get());
+  if (!exp)
+    throw std::runtime_error(
+        "ir_builder error: StmtAST expects ExpAST at param `exp`");
+
+  // Translate the expression inside the return
+  auto *val = translate_exp_c_ast(*exp, block);
+
+  return block.Make<koopa_ast::Return>(true, val);
+}
+
+/**
+ * Converting a Exp C AST to koopa
+ */
+koopa_ast::Value *translate_exp_c_ast(const c_ast::ExpAST &exp,
+                                      koopa_ast::BasicBlock &block) {
+  auto *add_exp = dynamic_cast<const c_ast::AddExpAST *>(exp.add_exp.get());
+  if (!add_exp)
+    throw std::runtime_error(
+        "ir_builder error: ExpAST expects AddExpAST at param `add_exp`");
+  return translate_add_exp_c_ast(*add_exp, block);
+}
+
+/**
+ * Converting a PrimaryExp in C AST to koopa
+ */
 koopa_ast::Value *translate_primary_exp_c_ast(const c_ast::PrimaryAST &primary,
                                               koopa_ast::BasicBlock &block) {
   if (auto *p = dynamic_cast<const c_ast::PrimaryASTExp *>(&primary)) {
@@ -61,6 +190,16 @@ koopa_ast::Value *translate_primary_exp_c_ast(const c_ast::PrimaryAST &primary,
         "ir_builder error: PrimaryAST must have one of the following "
         "implementation: {PrimaryASTExp, PrimaryASTNumber} ");
   }
+}
+
+/**
+ * Going from NumberAST to koopa Integer
+ */
+// std::unique_ptr<koopa_ast::Integer>
+koopa_ast::Integer *translate_number_c_ast(const c_ast::NumberAST &number,
+                                           koopa_ast::BasicBlock &block) {
+  auto *ret = block.Make<koopa_ast::Integer>(false, number.int_val);
+  return ret;
 }
 
 koopa_ast::Value *translate_unary_exp_c_ast(const c_ast::UnaryExpAST &unary,
@@ -119,135 +258,97 @@ koopa_ast::Value *translate_unary_exp_c_ast(const c_ast::UnaryExpAST &unary,
   }
 }
 
-koopa_ast::Value *translate_exp_c_ast(const c_ast::ExpAST &exp,
-                                      koopa_ast::BasicBlock &block) {
-  auto *unary_exp =
-      dynamic_cast<const c_ast::UnaryExpAST *>(exp.add_exp.get());
-  if (!unary_exp)
+koopa_ast::Value *translate_mul_exp_c_ast(const c_ast::MulExpAST &mul,
+                                          koopa_ast::BasicBlock &block) {
+
+  if (auto *p = dynamic_cast<const c_ast::MulExpASTUnary *>(&mul)) {
+    auto *unary = dynamic_cast<const c_ast::UnaryExpAST *>(p->unary_exp.get());
+    if (!unary)
+      throw std::runtime_error("ir_builder error: MulExpASTUnary expects "
+                               "UnaryAST at param `unary_exp`");
+    return translate_unary_exp_c_ast(*unary, block);
+  } else if (auto *p = dynamic_cast<const c_ast::MulExpASTMulUnary *>(&mul)) {
+
+    // Obtain the sub-node
+    auto *mul_exp = dynamic_cast<const c_ast::MulExpAST *>(p->mul_exp.get());
+    if (!mul_exp)
+      throw std::runtime_error("ir_builder error: MulExpASTUnary expects "
+                               "MulExpAST at param `mul_exp`");
+    auto *unary_exp =
+        dynamic_cast<const c_ast::UnaryExpAST *>(p->unary_exp.get());
+    if (!unary_exp)
+      throw std::runtime_error("ir_builder error: MulExpASTMulUnary expects "
+                               "UnaryAST at param `unary_exp`");
+
+    // Recursively parse the tree
+    auto *mul_exp_ast = translate_mul_exp_c_ast(*mul_exp, block);
+    auto *unary_exp_ast = translate_unary_exp_c_ast(*unary_exp, block);
+
+    // Build the instruction in koopa
+    switch (p->mul_op) {
+    case c_ast::MulOp::STAR: {
+      return block.Make<koopa_ast::Binary>(true, koopa_ast::BinaryOp::Mul,
+                                           mul_exp_ast, unary_exp_ast);
+    }
+    case c_ast::MulOp::SLASH: {
+      return block.Make<koopa_ast::Binary>(true, koopa_ast::BinaryOp::Div,
+                                           mul_exp_ast, unary_exp_ast);
+    }
+    case c_ast::MulOp::PERCENT: {
+      return block.Make<koopa_ast::Binary>(true, koopa_ast::BinaryOp::Mod,
+                                           mul_exp_ast, unary_exp_ast);
+    }
+    }
+  } else {
     throw std::runtime_error(
-        "ir_builder error: ExpAST expects UnaryExpAST at param `unary_exp`");
-  return translate_unary_exp_c_ast(*unary_exp, block);
-}
-
-/**
- * Going from StmtAST to koopa stmt
- *
- */
-koopa_ast::Value *translate_stmt_c_ast(const c_ast::StmtAST &stmt,
-                                       koopa_ast::BasicBlock &block) {
-  // Cast and check that it has the correct type
-  auto *exp = dynamic_cast<const c_ast::ExpAST *>(stmt.exp.get());
-  if (!exp)
-    throw std::runtime_error(
-        "ir_builder error: StmtAST expects ExpAST at param `exp`");
-
-  // Translate the expression inside the return
-  auto *val = translate_exp_c_ast(*exp, block);
-
-  return block.Make<koopa_ast::Return>(true, val);
-}
-
-/**
- * Converting FuncType C AST to just Type koopa IR reps.
- *
- * HACK: Currently since we don't have type for the c_ast,
- * this function will always return i32.
- */
-std::unique_ptr<koopa_ast::Type>
-translate_func_type_c_ast(const c_ast::FuncTypeAST &func_type) {
-  // HACK: Currently it is just hard matching as it has to be int
-  return std::make_unique<koopa_ast::Type>(koopa_ast::Type::I32());
-}
-
-/**
- * Converting a Block C AST to koopa Block for now.
- *
- * NOTE: The meaning of a block in C and a block in koopa is different.
- */
-std::unique_ptr<koopa_ast::BasicBlock>
-translate_block_c_ast(const c_ast::BlockAST &block, std::string name) {
-  auto ret = std::make_unique<koopa_ast::BasicBlock>(name);
-
-  auto *stmt = dynamic_cast<const c_ast::StmtAST *>(block.stmt.get());
-  if (!stmt)
-    throw std::runtime_error(
-        "ir_builder error: BlockAST expects to have StmtAST at param `stmt`");
-
-  translate_stmt_c_ast(*stmt, *ret.get());
-
-  return ret;
-}
-
-/**
- * Converting a FuncDefAST to a koopa function as the following:
- *
- *
- * ```
- *
- *  c_ast::FuncDefAST:name (string)
- *    -> koopa_ast::Function:name (string)
- *
- *  c_ast::FuncDefAST:type (string)
- *    -> koopa_ast::Function:type (koopa_ast::Type)
- *
- *  c_ast::FuncDefAST:block (c_ast::BlockAST)
- *    -> koopa_ast::Function:basicblocks (vector<koopa_ast::BasicBlock>)
- *
- * ```
- *
- *
- * NOTE: I think this does satisfy the definition of a koopa block as it does
- * only have one point of entry and only leave or return. However, this may not
- * be the most optimised way to write this, of course.
- */
-std::unique_ptr<koopa_ast::Function>
-translate_func_def_c_ast(const c_ast::FuncDefAST &func_def) {
-  auto ret = std::make_unique<koopa_ast::Function>();
-
-  // Get the function name
-  ret->name = "@" + func_def.ident;
-
-  // Get the type
-  auto *type =
-      dynamic_cast<const c_ast::FuncTypeAST *>(func_def.func_type.get());
-  if (!type) {
-    throw std::runtime_error("ir_builder error: FuncDefAST expects "
-                             "FuncTypeAST at param `func_type`");
+        "ir_builder error: MulExpAST must have one of the following "
+        "implementation: {MulExpASTUnary, MulExpASTMulUnary} ");
   }
-
-  ret->type = translate_func_type_c_ast(*type);
-
-  // Get the block
-  auto *block = dynamic_cast<const c_ast::BlockAST *>(func_def.block.get());
-  if (!block) {
-    throw std::runtime_error(
-        "ir_builder error: FuncDefAST expects BlockAST at param `block`");
-  }
-
-  // HACK: We only have one block for now.
-  ret->basicblocks.push_back(translate_block_c_ast(*block, "%entry"));
-
-  return ret;
 }
 
-/**
- * Converting a CompUnitAST in C to a program in koopa.
- */
-std::unique_ptr<koopa_ast::Program>
-translate_comp_unit_c_ast(const c_ast::CompUnitAST &comp_unit) {
-  auto ret = std::make_unique<koopa_ast::Program>();
+koopa_ast::Value *translate_add_exp_c_ast(const c_ast::AddExpAST &add,
+                                          koopa_ast::BasicBlock &block) {
+  if (auto *p = dynamic_cast<const c_ast::AddExpASTMul *>(&add)) {
+    auto *mul_exp = dynamic_cast<const c_ast::MulExpAST *>(p->mul_exp.get());
+    if (!mul_exp)
+      throw std::runtime_error("ir_builder error: AddExpASTMul expects "
+                               "MulExpAST at param `mul_exp`");
 
-  auto *func_def =
-      dynamic_cast<const c_ast::FuncDefAST *>(comp_unit.func_def.get());
-  if (!func_def) {
+    return translate_mul_exp_c_ast(*mul_exp, block);
+  } else if (auto *p = dynamic_cast<const c_ast::AddExpASTAddMul *>(&add)) {
+    // Obtain the sub-node
+    auto *add_exp = dynamic_cast<const c_ast::AddExpAST *>(p->add_exp.get());
+    if (!add_exp)
+      throw std::runtime_error("ir_builder error: AddExpASTAddMul expects "
+                               "AddExpAST at param `add_exp`");
+    auto *mul_exp = dynamic_cast<const c_ast::MulExpAST *>(p->mul_exp.get());
+    if (!mul_exp)
+      throw std::runtime_error("ir_builder error: AddExpASTAddMul expects "
+                               "MulExpAST at param `mul_exp`");
+
+    // Recursively parse the tree
+    auto *add_exp_ast = translate_add_exp_c_ast(*add_exp, block);
+    auto *mul_exp_ast = translate_mul_exp_c_ast(*mul_exp, block);
+
+    // Build the instruction in koopa
+    switch (p->add_op) {
+    case c_ast::AddOp::PLUS:
+      return block.Make<koopa_ast::Binary>(true, koopa_ast::BinaryOp::Add,
+                                           add_exp_ast, mul_exp_ast);
+    case c_ast::AddOp::MINUS:
+      return block.Make<koopa_ast::Binary>(true, koopa_ast::BinaryOp::Sub,
+                                           add_exp_ast, mul_exp_ast);
+    }
+  } else {
     throw std::runtime_error(
-        "ir_builder error: CompUnitAST expects FuncDefAST at param `func_def`");
+        "ir_builder error: AddExpAST must have one of the following "
+        "implementation: {AddExpASTMul, AddExpASTAddMul} ");
   }
-
-  ret->functions.push_back(translate_func_def_c_ast(*func_def));
-
-  return ret;
 }
+
+/*******************************************************************************
+ *  Public API Implementation for translation.                                 *
+ *******************************************************************************/
 
 /*
  * Exposed API for converting from C AST to Koopa Representation
