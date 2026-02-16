3ba79543b035c69299e38de91526fe30757309db
Isaac Leong <isaacleong23367@gmail.com>
2026-02-16 18:27:48 +1100

    Feat: Extended Abstract Layer of RISC-V Assembly

--- rust/src/riscv.rs
+++ rust/src/riscv.rs
@@ -10,8 +10,14 @@ pub enum AsmInst {
     Mul { rd: Reg, rs1: Reg, rs2: Reg },
     Div { rd: Reg, rs1: Reg, rs2: Reg },
     Mod { rd: Reg, rs1: Reg, rs2: Reg },
+    And { rd: Reg, rs1: Reg, rs2: Reg },
+    Or { rd: Reg, rs1: Reg, rs2: Reg },
     Xor { rd: Reg, rs1: Reg, rs2: Reg },
+    Xori { rd: Reg, rs: Reg, imm: i16 },
+    Slt { rd: Reg, rs1: Reg, rs2: Reg },
+    Sgt { rd: Reg, rs1: Reg, rs2: Reg },
     Seqz { rd: Reg, rs: Reg },
+    Snez { rd: Reg, rs: Reg },
     Ret,
 }
 
@@ -23,6 +29,7 @@ impl Display for AsmInst {
             AsmInst::Li { rd, imm }       => write!(f, "\tli    {rd}, {imm}"),
             AsmInst::Mv { rd, rs }        => write!(f, "\tmv    {rd}, {rs}"),
             AsmInst::Seqz { rd, rs }      => write!(f, "\tseqz  {rd}, {rs}"),
+            AsmInst::Snez { rd, rs }      => write!(f, "\tsnez  {rd}, {rs}"),
             // Control flow instructions
             AsmInst::Ret                  => write!(f, "\tret"),
             // Arithmetic Instructions
@@ -31,7 +38,12 @@ impl Display for AsmInst {
             AsmInst::Mul { rd, rs1, rs2 } => write!(f, "\tmul   {rd}, {rs1}, {rs2}"),
             AsmInst::Div { rd, rs1, rs2 } => write!(f, "\tdiv   {rd}, {rs1}, {rs2}"),
             AsmInst::Mod { rd, rs1, rs2 } => write!(f, "\trem   {rd}, {rs1}, {rs2}"),
+            AsmInst::And { rd, rs1, rs2 } => write!(f, "\tand   {rd}, {rs1}, {rs2}"),
+            AsmInst::Or { rd, rs1, rs2 }  => write!(f, "\tor    {rd}, {rs1}, {rs2}"),
             AsmInst::Xor { rd, rs1, rs2 } => write!(f, "\txor   {rd}, {rs1}, {rs2}"),
+            AsmInst::Xori { rd, rs, imm } => write!(f, "\txori  {rd}, {rs}, {imm}"),
+            AsmInst::Slt { rd, rs1, rs2 } => write!(f, "\tslt   {rd}, {rs1}, {rs2}"),
+            AsmInst::Sgt { rd, rs1, rs2 } => write!(f, "\tslt   {rd}, {rs2}, {rs1}"),
         }
     }
 }
